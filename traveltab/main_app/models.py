250b4d74c4071227e38b87c5773e458319179cae
ryanqle <ryan.q.le@gmail.com>
2023-05-06 15:18:49 -0500

    Stub models

--- traveltab/main_app/models.py
+++ traveltab/main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Trip(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Member(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    name = models.CharField()
+
+class Transaction(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateField()
+    payer = models.ForeignKey(Member, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
