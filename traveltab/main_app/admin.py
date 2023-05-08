56f23a31a5490ff0152e91b4104b7f182465268f
ryanqle <ryan.q.le@gmail.com>
2023-05-08 12:54:57 -0500

    Custom user creation form to include name/ email

--- traveltab/main_app/admin.py
+++ traveltab/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Trip, Member, Transaction
 
 # Register your models here.
+admin.site.register(Trip)
+admin.site.register(Member)
+admin.site.register(Transaction)
--- traveltab/main_app/views.py
+++ traveltab/main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.forms.forms import Form
+from django import forms  
 
 # Create your views here.
 def home(request):
@@ -10,16 +12,23 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(label='Username', min_length=5, max_length=150, required=True)  
+    email = forms.EmailField(label='Email',required=True)  
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)  
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.') 
+
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = CustomUserCreationForm(request.POST)
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('index')
+      return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
+  form = CustomUserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
