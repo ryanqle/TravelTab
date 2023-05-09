17d7c34b5043952c943c1e87f3755ba87c400108
ryanqle <ryan.q.le@gmail.com>
2023-05-08 23:15:15 -0500

    Create New Trip

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('trips/', views.user_trips, name='index'),
+    path('trips/create/', views.TripCreate.as_view(), name='trips_create')
 ]
